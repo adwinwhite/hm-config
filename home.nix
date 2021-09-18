87494b732e5f703d16dc7b7f599c018951efb587
AdwinKing <jin2000112014@gmail.com>
2021-09-18 16:18:49 +0800

    neovim: fixed formatter for golang

--- home.nix
+++ home.nix
@@ -189,6 +189,7 @@
                 return {
                   exe = "gofmt",
                   args = { vim.api.nvim_buf_get_name(0)},
+                  stdin = true
                 }
               end
             },
