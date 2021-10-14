972e58dca5f00442fb667c1e8e2440138556c880
AdwinKing <jin2000112014@gmail.com>
2021-10-14 11:58:01 +0800

    neovim: disable snippet completion except for latex

--- home.nix
+++ home.nix
@@ -93,11 +93,16 @@
         let g:completion_enable_snippet = 'vim-vsnip'
         let g:completion_chain_complete_list = {
             \ 'default': [
+            \    {'complete_items': ['lsp', 'path']},
+            \    {'mode': '<c-p>'},
+            \    {'mode': '<c-n>'}
+            \],
+            \ 'TelescopePrompt' : [ ],
+            \ 'latex': [
             \    {'complete_items': ['lsp', 'path', 'snippet']},
             \    {'mode': '<c-p>'},
             \    {'mode': '<c-n>'}
             \],
-            \ 'TelescopePrompt' : [ ]
         \}
         let g:completion_auto_change_source = 1
         let g:completion_matching_ignore_case = 1
